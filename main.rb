82a4fd4e2e2746e13345e0bb73586adf4094a972
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2014-10-18 12:21:20 +0200

    test_2_lives_zombie_dies_after_two_shots

--- main.rb
+++ main.rb
@@ -16,6 +16,14 @@ class ZombiakiTestCase < Test::Unit::TestCase
     assert_1_zombie_at_middle_street(app)
   end
 
+  def test_2_lives_zombie_dies_after_two_shots
+    app = ZombieGameApp.new
+    app.put_zombie_at_middle_street(two_lives_zombie)
+    app.make_shoot_at_middle_street()
+    app.make_shoot_at_middle_street()
+    assert_0_zombies_left(app)
+  end
+
   private
 
   def one_life_zombie
@@ -44,6 +52,10 @@ class Zombie
   def one_life_left?
     @lives == 1
   end
+
+  def add_injury
+    @lives -= 1
+  end
 end
 
 class ZombieGameApp
@@ -58,6 +70,8 @@ class ZombieGameApp
   def make_shoot_at_middle_street
     if @zombies_at_middle_street.first.one_life_left?
       @zombies_at_middle_street.delete(@zombies_at_middle_street.last)
+    else
+      @zombies_at_middle_street.last.add_injury
     end
   end
 
