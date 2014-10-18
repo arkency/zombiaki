3472f459acbea09470423d43ad94be693d2d57fb
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2014-10-18 12:10:22 +0200

    When Zombie is on the Street and Shot on the Street is made, then Zombie dies.

--- main.rb
+++ main.rb
@@ -2,6 +2,29 @@ require 'test/unit'
 
 class ZombiakiTestCase < Test::Unit::TestCase
   def test_main
-    assert_equal(true, true)
+    app = ZombieGameApp.new
+    app.put_zombie_at_middle_street("wladek")
+    assert_equal(1, app.zombies_at_middle_street().count)
+    app.make_shoot_at_middle_street()
+    assert_equal(0, app.zombies_at_middle_street().count)
+  end
+end
+
+
+class ZombieGameApp
+  def initialize
+    @zombies_at_middle_street = []
+  end
+
+  def put_zombie_at_middle_street(zombie)
+    @zombies_at_middle_street << zombie
+  end
+
+  def make_shoot_at_middle_street
+    @zombies_at_middle_street.delete(@zombies_at_middle_street.last)
+  end
+
+  def zombies_at_middle_street
+    @zombies_at_middle_street
   end
 end
