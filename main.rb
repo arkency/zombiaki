c00a93b2e46680256c4c80750d742857034599e6
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2014-10-18 12:19:20 +0200

    make it more like a story

--- main.rb
+++ main.rb
@@ -3,16 +3,34 @@ require 'test/unit'
 class ZombiakiTestCase < Test::Unit::TestCase
   def test_zombie_dies_after_shot
     app = ZombieGameApp.new
-    app.put_zombie_at_middle_street(Zombie.new(lives=1))
-    assert_equal(1, app.zombies_at_middle_street().count)
+    app.put_zombie_at_middle_street(one_life_zombie)
+    assert_1_zombie_at_middle_street(app)
     app.make_shoot_at_middle_street()
-    assert_equal(0, app.zombies_at_middle_street().count)
+    assert_0_zombies_left(app)
   end
 
   def test_2_lives_zombie_survives_one_shot
     app = ZombieGameApp.new
-    app.put_zombie_at_middle_street(Zombie.new(lives=2))
+    app.put_zombie_at_middle_street(two_lives_zombie)
     app.make_shoot_at_middle_street()
+    assert_1_zombie_at_middle_street(app)
+  end
+
+  private
+
+  def one_life_zombie
+    Zombie.new(lives=1)
+  end
+
+  def two_lives_zombie
+    Zombie.new(lives=2)
+  end
+
+  def assert_0_zombies_left(app)
+    assert_equal(0, app.zombies_at_middle_street().count)
+  end
+
+  def assert_1_zombie_at_middle_street(app)
     assert_equal(1, app.zombies_at_middle_street().count)
   end
 end
