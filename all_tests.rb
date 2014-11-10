f0c9314157648cb7eabeab7a5d946c7fcb303bbd
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2014-11-10 01:57:25 +0100

    split test files

---
+++ all_tests.rb
@@ -0,0 +1,2 @@
+require './zombiaki_test'
+require './full_game_test'
---
+++ full_game_test.rb
@@ -0,0 +1,136 @@
+class FullGame < Test::Unit::TestCase
+  def test_zombie_win_by_getting_to_barricade
+    zombie_stack = Stack.new
+    wladek_1 = ThingAppearsOnPlace.new(Zombie.new(4, "wladek"))
+    wladek_2 = ThingAppearsOnPlace.new(Zombie.new(4, "wladek"))
+    griszka_1 = ThingAppearsOnPlace.new(Zombie.new(2, "griszka"))
+    griszka_2 = ThingAppearsOnPlace.new(Zombie.new(2, "griszka"))
+    zombie_stack << wladek_1
+    zombie_stack << wladek_2
+    zombie_stack << griszka_1
+    zombie_stack << griszka_2
+    zombie_stack << SteroidsEffect.new
+
+    humans_stack = Stack.new
+    shoot_1 = ShootEffect.new
+    shoot_2 = ShootEffect.new
+    shoot_3 = ShootEffect.new
+    shoot_4 = ShootEffect.new
+
+    humans_stack << shoot_1
+    humans_stack << shoot_2
+    humans_stack << shoot_3
+    humans_stack << shoot_4
+
+    game = ZombieGameApp.new(zombie_stack, humans_stack)
+
+    game.zombies_take_cards_to_hand
+    game.zombies_remove_card_to_trash(griszka_2)
+
+    game.zombies_play_card_on_place(wladek_1, 0, 5)
+    game.zombies_play_card_on_place(wladek_2, 2, 5)
+    game.zombies_finish_move
+
+    game.humans_take_cards_to_hand
+    game.humans_remove_card_to_trash(shoot_1)
+    game.humans_play_card(shoot_2, 0, 0)
+    game.humans_play_card(shoot_3, 1, 0)
+    game.humans_finish_move
+
+    game.play_zombies_turn
+    assert_equal(false, game.won_by_zombies?)
+
+    #zombies now at 4
+    game.zombies_take_cards_to_hand
+    game.zombies_finish_move
+
+    game.play_humans_turn
+    game.humans_take_cards_to_hand
+    game.humans_finish_move
+
+
+    game.play_zombies_turn
+    assert_equal(false, game.won_by_zombies?)
+    #zombies now at 3
+    game.zombies_take_cards_to_hand
+    game.zombies_finish_move
+
+    game.play_humans_turn
+    game.humans_take_cards_to_hand
+    game.humans_finish_move
+
+
+    game.play_zombies_turn
+    assert_equal(false, game.won_by_zombies?)
+    #zombies now at 2
+    game.zombies_take_cards_to_hand
+    game.zombies_finish_move
+
+    game.play_humans_turn
+    game.humans_take_cards_to_hand
+    game.humans_finish_move
+
+
+    game.play_zombies_turn
+    assert_equal(false, game.won_by_zombies?)
+    #zombies now at 1
+    game.zombies_take_cards_to_hand
+    game.zombies_finish_move
+
+    game.play_humans_turn
+    game.humans_take_cards_to_hand
+    game.humans_finish_move
+
+
+    game.play_zombies_turn
+    assert_equal(true, game.won_by_zombies?)
+    #zombies now at 0
+    game.zombies_take_cards_to_hand
+    game.zombies_finish_move
+
+    game.play_humans_turn
+    game.humans_take_cards_to_hand
+    game.humans_finish_move
+  end
+
+  def test_humans_win_because_dawn
+    zombie_stack = Stack.new
+    wladek_1 = ThingAppearsOnPlace.new(Zombie.new(4, "wladek"))
+    wladek_2 = ThingAppearsOnPlace.new(Zombie.new(4, "wladek"))
+    griszka_1 = ThingAppearsOnPlace.new(Zombie.new(2, "griszka"))
+    dawn = Dawn.new
+    zombie_stack << wladek_1
+    zombie_stack << wladek_2
+    zombie_stack << griszka_1
+    zombie_stack << dawn
+
+    humans_stack = Stack.new
+
+    game = ZombieGameApp.new(zombie_stack, humans_stack)
+
+    game.play_zombies_turn
+    assert_raises ZombieLost do
+      game.zombies_take_cards_to_hand
+    end
+  end
+
+  def test_zombies_only_start_at_5th_row
+    zombie_stack = Stack.new
+    wladek = ThingAppearsOnPlace.new(Zombie.new(4, "wladek"))
+    zombie_stack << wladek
+    zombie_stack << ThingAppearsOnPlace.new(Zombie.new(4, "wladek"))
+    zombie_stack << ThingAppearsOnPlace.new(Zombie.new(2, "griszka"))
+    zombie_stack << ThingAppearsOnPlace.new(Zombie.new(2, "griszka"))
+    zombie_stack << Dawn.new
+
+    humans_stack = Stack.new
+
+    game = ZombieGameApp.new(zombie_stack, humans_stack)
+
+    game.play_zombies_turn
+    assert_raises InvalidMove do
+      game.zombies_play_card_on_place(wladek, 0, 4)
+    end
+  end
+
+end
